c0ac99c45f0ad9bfb3359e6ecb7b9b43884532eb
Guillermo <roblesguillermolopez@gmail.com>
2025-03-20 08:44:50 +0100

    Carrito terminado

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -12,21 +12,23 @@ import {
 import { ExploreContainerComponent } from '../components/explore-container/explore-container.component';
 import {SharpService} from "../services/sharp/sharp.service";
 import {Product} from "../models/Product";
-import {CommonModule} from "@angular/common";
+import {CommonModule, CurrencyPipe} from "@angular/common";
 import {ProductCardComponent} from "../components/product-card/product-card.component";
 import {RouterLink} from "@angular/router";
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonItemSliding, RouterLink, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonText, CommonModule, ProductCardComponent],
+  imports: [IonHeader, CurrencyPipe, IonItemSliding, RouterLink, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonText, CommonModule, ProductCardComponent],
 })
 export class Tab1Page implements OnInit{
   sharpList: Array<Product> = [];
+  precioTotal?: number;
   constructor(private sharpService: SharpService) {}
   ngOnInit() {
     this.sharpService.currentSharp.subscribe((data) => {
       this.sharpList = data;
+      this.precioTotal = this.sharpService.getPrecioTotal();
     })
   }
 }
