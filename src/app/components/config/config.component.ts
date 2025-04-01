765a29e46cc0c970debc0373bb03a9123da151ba
Guillermo <roblesguillermolopez@gmail.com>
2025-04-01 12:39:06 +0200

    Add contact form modal and enhance personal data modal functionality

--- src/app/components/config/config.component.ts
+++ src/app/components/config/config.component.ts
@@ -1,38 +1,42 @@
 import {Component, inject, OnInit, ViewChild} from '@angular/core';
 import {
+  IonAlert,
   IonAvatar,
   IonButton, IonButtons,
   IonCard, IonCardContent,
   IonCardHeader, IonCardSubtitle,
   IonCardTitle,
   IonContent,
-  IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonSelect, IonSelectOption,
+  IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonSelect, IonSelectOption, IonText,
   IonTitle, IonToggle,
   IonToolbar
 } from "@ionic/angular/standalone";
 import {addIcons} from "ionicons";
-import {personOutline, trendingUpOutline, logOutOutline, moonOutline, languageOutline} from "ionicons/icons";
+import {personOutline, call, person, people, mail, trendingUpOutline, logOutOutline, moonOutline, languageOutline, personAddOutline} from "ionicons/icons";
 import {User} from "../../models/User";
 import {Router, RouterLink} from "@angular/router";
 import {AuthService} from "../../services/auth/auth.service";
 import {CustomHeaderComponent} from "../custom-header/custom-header/custom-header.component";
 import {IonModal} from "@ionic/angular/standalone";
 import { OverlayEventDetail } from '@ionic/core/components';
-import {FormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {FirebaseService} from "../../services/firebase-service/firebase.service";
 import {TranslocoModule, TranslocoService} from "@ngneat/transloco";
 import {TranslocoHttpLoader} from "../../../transloco-loader";
 import {CommonModule} from "@angular/common";
 import {LocalStorageService} from "../../services/local-storage/local-storage.service";
 import {PaletteService} from "../../services/palette/palette.service";
+import {IonicModule} from "@ionic/angular";
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
   styleUrls: ['./config.component.scss'],
-  imports: [IonHeader, CommonModule, IonSelect, IonSelectOption, TranslocoModule, IonToggle, FormsModule, IonButtons, IonModal, IonInput, CustomHeaderComponent, IonIcon, RouterLink, IonList, IonItem, IonLabel, IonToggle, IonToolbar, IonTitle, IonContent, IonCard, IonAvatar, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton],
+  imports: [IonHeader, ReactiveFormsModule, IonText, IonAlert, CommonModule, IonSelect, IonSelectOption, TranslocoModule, IonToggle, FormsModule, IonButtons, IonModal, IonInput, CustomHeaderComponent, IonIcon, RouterLink, IonList, IonItem, IonLabel, IonToggle, IonToolbar, IonTitle, IonContent, IonCard, IonAvatar, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton],
 })
-export class ConfigComponent  implements OnInit {
-  @ViewChild(IonModal) modal!: IonModal;
+export class ConfigComponent implements OnInit {
+  @ViewChild('personalDataModal') personalDataModal!: IonModal;
+  @ViewChild('contactModal') contactModal!: IonModal;
+
   private authService: AuthService = inject(AuthService);
   private firestoreService: FirebaseService = inject(FirebaseService);
   private translocoService: TranslocoService = inject(TranslocoService);
@@ -47,8 +51,18 @@ export class ConfigComponent  implements OnInit {
   user: User | null = null;
   firstName: string = this.authService.getFirstName();
   isDark: boolean = false;
-  constructor(private router: Router) {
-    addIcons({personOutline, trendingUpOutline, logOutOutline, moonOutline, languageOutline});
+  formulario: FormGroup;
+  private phonePattern: RegExp = /^[0-9]{9}$/;
+  showAlert: boolean = false;
+
+  constructor(private router: Router, private form: FormBuilder) {
+    this.formulario = this.form.group({
+      nombre: ['', [Validators.required]],
+      apellidos: ['', [Validators.required]],
+      telefono: ['', [Validators.required, Validators.pattern(this.phonePattern)]],
+      email: ['', [Validators.required, Validators.email]],
+    })
+    addIcons({personOutline, call, person, people, mail, trendingUpOutline, logOutOutline, moonOutline, languageOutline, personAddOutline});
   }
 
   ngOnInit() {
@@ -62,6 +76,18 @@ export class ConfigComponent  implements OnInit {
       this.invertedLanguageMap.set(value, key);
     })
     this.currentLanguage = this.languageMap.get(this.localStorageService.getItem('selectedLanguage') || 'es') || 'es';
+  }
+
+  hasErrors(controlName: string, errorName: string): boolean | undefined {
+    return this.formulario.get(controlName)?.hasError(errorName) && this.formulario.get(controlName)?.touched
+  }
+
+  enviar(): void {
+    if (this.formulario.invalid) {
+      this.formulario.markAllAsTouched();
+      return;
+    }
+    this.showAlert = true;
   }
 
   changeLanguage(event: CustomEvent): void  {
@@ -78,7 +104,7 @@ export class ConfigComponent  implements OnInit {
 
   confirm(): void {
     if(this.firstName.length > 0){
-      this.modal.dismiss('confirm');
+      this.personalDataModal.dismiss('confirm');
       const uid: string = this.authService.getUid();
       this.firestoreService.updateUserName(uid, this.firstName).then(() => {
         if (this.user) {
@@ -86,7 +112,7 @@ export class ConfigComponent  implements OnInit {
         }
       });
     }else{
-      this.modal.dismiss('confirm');
+      this.personalDataModal.dismiss('confirm');
     }
   }
 
@@ -95,11 +121,22 @@ export class ConfigComponent  implements OnInit {
   }
 
   cancel(): void {
-    this.modal.dismiss(null, 'cancel');
+    this.personalDataModal.dismiss(null, 'cancel');
   }
 
-  onWillDismiss(event: CustomEvent<OverlayEventDetail>) {
+  confirmContactModal(): void {
+    if (this.formulario.invalid) {
+      this.formulario.markAllAsTouched();
+      return;
+    }
+    this.showAlert = true;
+    setTimeout(() => {
+      this.contactModal.dismiss(null, 'confirm');
+    }, 500);
+  }
 
+  cancelContactModal(): void {
+    this.contactModal.dismiss(null, 'cancel');
   }
 
   logOut(): void{
@@ -109,13 +146,4 @@ export class ConfigComponent  implements OnInit {
     this.paletteService.toogleDarkPalette(false);
     this.authService.logout().then((): void => {this.router.navigate(['/login'])});
   }
-
-  navigateToOrders(): void {
-
-  }
-
-  changeName(): void{
-
-  }
-
 }
