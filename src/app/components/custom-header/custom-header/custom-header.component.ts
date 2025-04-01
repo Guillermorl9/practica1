36dbcc75b1333b8b5ee415f297297177971b311d
Guillermo <roblesguillermolopez@gmail.com>
2025-04-01 09:10:23 +0200

    Mejorar la interactividad y la accesibilidad de los componentes de la interfaz

--- src/app/components/custom-header/custom-header/custom-header.component.ts
+++ src/app/components/custom-header/custom-header/custom-header.component.ts
@@ -1,12 +1,12 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {IonBackButton, IonButtons, IonHeader, IonTitle, IonToolbar} from "@ionic/angular/standalone";
+import {IonAvatar, IonBackButton, IonButtons, IonHeader, IonTitle, IonToolbar} from "@ionic/angular/standalone";
 import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-custom-header',
   templateUrl: './custom-header.component.html',
   styleUrls: ['./custom-header.component.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonButtons, IonBackButton, CommonModule]
+  imports: [IonHeader, IonAvatar, IonToolbar, IonTitle, IonButtons, IonBackButton, CommonModule]
 })
 export class CustomHeaderComponent  implements OnInit {
   @Input() title?: string;
